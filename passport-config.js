0b89dcc4ca9df9fc894df146844cfd3d3124799b
Ricardo Bastos <ricardo.bastos@vicaima-sgps.pt>
2023-02-01 17:01:17 +0000

    Login logic + routes completed

--- passport-config.js
+++ passport-config.js
@@ -1,8 +1,8 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
-function initialize(passport, getUserByEmail) {
-    const authenticatedUser = async (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email);
         if (user == null) {
             return done(null, false, { message: 'No user with that email' });
@@ -17,11 +17,14 @@ function initialize(passport, getUserByEmail) {
         } catch (e) {
             return done(e);
         }
+
     }
-    
-    passport.use(new LocalStrategy({ usernameField: 'email' }), authenticatedUser);
-    passport.serializeUser((user, done) => { });
-    passport.deserializeUser((id, done) => { });
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id));
+    });
 }
 
 module.exports = initialize;
